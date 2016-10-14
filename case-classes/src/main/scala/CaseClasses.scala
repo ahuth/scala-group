91e057d5cf857557c577656f75e4cd290a8ca57d
Andrew <ahuth@mavenlink.com>
2016-10-13 22:30:48 -0700

    Add implementation for `CaseClasses`

--- case-classes/src/main/scala/CaseClasses.scala
+++ case-classes/src/main/scala/CaseClasses.scala
@@ -4,15 +4,18 @@ case class Location(city: String, country: String)
 case class Person(name: String, location: Location)
 
 object CaseClasses {
-  def extract_name(peep: Person): String = {
-    ""
+  def extract_name(peep: Person): String = peep match {
+    case Person(name, _) => name
   }
 
-  def is_canadian(peep: Person): Boolean = {
-    false
+  def is_canadian(peep: Person): Boolean = peep match {
+    case Person(_, Location(_, "Canada")) => true
+    case _ => false
   }
 
-  def names_from(people: List[Person], country: String): List[String] = {
-    List.empty[String]
+  def names_from(people: List[Person], country: String): List[String] = people match {
+    case Nil => List.empty[String]
+    case Person(name, Location(_, `country`)) :: tail => name :: names_from(tail, country)
+    case _ :: tail => names_from(tail, country)
   }
 }
